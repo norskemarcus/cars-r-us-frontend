a16f6d3532c17e11a328876a8bca9c49fefc998c
norskemarcus <marc791j@stud.kea.dk>
2023-03-14 21:23:43 +0100

    changed to https

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://carsareusmar.azurewebsites.net/api";
+export const API_URL = "https://carsareusmar.azurewebsites.net/api";
 
 export const FETCH_NO_API_ERROR =
   " (Is the API online or did the endpoint exists ?)";
